112dd60b4b1692e68b3a666c0fb5964397b5f88f
Robert Bogart <robert.bogart@joyent.com>
2019-11-06 17:54:37 +0000

    MANTA-4686 Rebalancer agent should create its own assignments for unit testing

--- src/util.rs
+++ src/util.rs
@@ -8,12 +8,31 @@
  * Copyright 2019, Joyent, Inc.
  */
 
+use std::fs;
 use std::io;
 use std::sync::Mutex;
 use std::thread;
 
 use clap::{crate_name, crate_version};
+use md5::{Digest, Md5};
 use slog::{o, Drain, Logger};
+
+pub fn calculate_file_md5(file_path: &str) -> String {
+    let mut file = match fs::File::open(&file_path) {
+        Err(e) => panic!("Error opening file {}", e),
+        Ok(file) => file,
+    };
+
+    let mut hasher = Md5::new();
+    match std::io::copy(&mut file, &mut hasher) {
+        Ok(_) => (),
+        Err(e) => {
+            panic!("Error hashing {}", e);
+        }
+    };
+
+    base64::encode(&hasher.result())
+}
 
 pub fn create_bunyan_logger<W>(io: W) -> Logger
 where
