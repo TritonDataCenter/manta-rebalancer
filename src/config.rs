6e9f993f03c13cc90fdf6dd7f8125ef4724009ce
Robert Bogart <robert.bogart@joyent.com>
2019-12-03 20:57:11 +0000

    MANTA-4766 Separate rebalancer agent deliverable from the rebalancer zone

--- src/config.rs
+++ src/config.rs
@@ -283,3 +283,9 @@ fn job_create_subcommand_handler(
 
     Ok(SubCommand::DoJob(Box::new(job)))
 }
+
+pub fn print_version() {
+    let version = env!("CARGO_PKG_VERSION");
+    let name = env!("CARGO_PKG_NAME");
+    println!("{} {}", name, version);
+}
