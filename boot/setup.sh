441313f54f5dd1bb73afc1f97e9fdbd8a9208d3c
Trent Mick <trentm@gmail.com>
2019-12-17 01:20:19 +0000

    first stab at sapi manifest; fix SMF manifest; attempt to use errexit in setup.sh

--- boot/setup.sh
+++ boot/setup.sh
@@ -11,17 +11,13 @@
 
 export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -o xtrace
-# XXX Manta boot/setup.sh scripts don't typically have errexit? Can we get away with it?
-#set -o errexit
+# Dev Note: Attempt to get away with using 'errexit'. I'm not sure how ready
+# the manta-scripts are for this.
+set -o errexit
 
 source /opt/smartdc/boot/scripts/util.sh
 source /opt/smartdc/boot/scripts/services.sh
 
-#XXX not sure this is necessary
-export PATH=/opt/smartdc/rebalancer/bin:/opt/local/bin:/usr/sbin:/usr/bin:$PATH
-
-
-# ---- mainline
 
 manta_common_presetup
 manta_add_manifest_dir "/opt/smartdc/rebalancer"
