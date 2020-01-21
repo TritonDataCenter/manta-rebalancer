f6fc2d45a3c0f004b7feb9bb67aefe6b424177b8
Rui Loura <rui@joyent.com>
2020-01-21 13:30:01 -0500

    clean up get_thread_name()

--- src/util.rs
+++ src/util.rs
@@ -34,20 +34,25 @@ pub fn init_global_logger() -> slog_scope::GlobalLoggerGuard {
     slog_scope::set_global_logger(log)
 }
 
+// If no name just print the crate name with the thread ID like so:
+//      "<crate_name>[ThreadId(<tid>)]: <log msg>"
+// Otherwise add the thread name as well.
+//      "<crate_name>[ThreadId(<tid>):<thread name>]: <log msg>"
 pub fn get_thread_name() -> String {
-    let cn = crate_name!().to_owned();
-    let tid = thread::current().id();
+    let output = format!(
+        "{}[{:?}",
+        crate_name!().to_owned(),
+        thread::current().id()
+    );
 
-    // If no name just print the crate name with the thread ID like so:
-    //      "<crate_name>[ThreadId(<tid>)]: <log msg>"
-    if thread::current().name().is_none() {
-        let ret = format!("{}[{:?}]", cn, tid);
-        return ret;
+    match thread::current().name() {
+        Some(thn) => {
+            format!("{}:{}]", output, thn)
+        },
+        None => {
+            format!("{}]", output)
+        }
     }
-
-    // Prepend the crate name to the thread ID and the thread name.
-    //      "<crate_name>[ThreadId(<tid>):<thread name>]: <log msg>"
-    format!("{}[{:?}:{}]", &cn, tid, thread::current().name().unwrap())
 }
 
 #[macro_export]
