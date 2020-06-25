1cac8dcde79569c198978642948e2a8e20f0688d
Rui Loura <rui@joyent.com>
2020-06-25 10:18:21 -0400

    MANTA-5329 /opt/local/bin should also be in the rebalancer zone's path

--- boot/setup.sh
+++ boot/setup.sh
@@ -18,10 +18,12 @@ set -o errexit
 source /opt/smartdc/boot/scripts/util.sh
 source /opt/smartdc/boot/scripts/services.sh
 
-
 manta_common_presetup
 manta_add_manifest_dir "/opt/smartdc/rebalancer"
 manta_common2_setup "rebalancer"
+
+# Set path for rebalancer-adm
+echo "export PATH=\$PATH:/opt/smartdc/rebalancer/bin" >> /root/.bashrc
 
 echo "Setting up rebalancer manager"
 /usr/sbin/svccfg import /opt/local/lib/svc/manifest/postgresql.xml
@@ -31,8 +33,5 @@ cp /opt/smartdc/rebalancer/etc/postgresql.conf /var/pgsql/data/postgresql.conf
 
 manta_common2_setup_log_rotation "rebalancer"
 manta_common2_setup_end
-
-# Set path for rebalancer-adm
-echo "export PATH=$PATH:/opt/smartdc/rebalancer/bin" >> /root/.bashrc
 
 exit 0
